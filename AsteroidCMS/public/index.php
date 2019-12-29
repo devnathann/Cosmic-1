c5db9cc3385a9559b3268b63baf0951ae38ffdc5
Raizer <devraizer@asteroidcms.nl>
2019-12-29 03:25:27 +0100

    installation fixes

--- AsteroidCMS/public/index.php
+++ AsteroidCMS/public/index.php
@@ -3,20 +3,17 @@ use App\Config;
 use Core\Routes;
 use Core\QueryBuilder;
 
-if (file_exists(__DIR__ . '/../vendor/usmanhalalit/') && file_exists(__DIR__ . '/../vendor/twig/') || !file_exists(__DIR__ . '/../App/Config.php')) {
+if (file_exists(__DIR__ . '/../vendor/usmanhalalit/') && file_exists(__DIR__ . '/../vendor/twig/')) {
     if (!file_exists(__DIR__ . '/uploads/')) {
         mkdir(__DIR__ . '/uploads/', 0777, true);
     } elseif (!file_exists(__DIR__ . '/tmp/')) {
         mkdir(__DIR__ . '/tmp/', 0777, true);
     }
 
-    require_once __DIR__ . '/../vendor/autoload.php';
 } else {
     echo 'Please update composer, vendors are missing!';
     exit;
 }
-
-include __DIR__ . '/../Core/Helper.php';
 
 if(!file_exists(__DIR__ . '/../App/Config.php')) {
     $copy = copy(\App\Models\Install::$tmp, \App\Models\Install::$path);
@@ -24,6 +21,10 @@ if(!file_exists(__DIR__ . '/../App/Config.php')) {
         redirect('/');
     }
 }
+
+require_once __DIR__ . '/../Core/Helper.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
 
 if(Config::debug) {
     ini_set("display_errors", 1);
