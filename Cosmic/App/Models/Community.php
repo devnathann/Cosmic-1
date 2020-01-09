aab8204b7163c4c1b86f02ab77a9eb5933bea496
Raizer <devraizer@asteroidcms.nl>
2020-01-09 02:20:53 +0100

    hide admins from highscores

--- Cosmic/App/Models/Community.php
+++ Cosmic/App/Models/Community.php
@@ -177,17 +177,17 @@ class Community
   
     public static function getAchievement($limit = 10)
     {
-        return  QueryBuilder::table('users_settings')->select('user_id')->select('achievement_score')->orderBy('achievement_score', 'desc')->limit($limit)->get();
+        return  QueryBuilder::table('users_settings')->select('user_id')->select('achievement_score')->join('users', 'users_settings.id', '=', 'users.id')->where('users.rank', '<', 4)->orderBy('achievement_score', 'desc')->limit($limit)->get();
     }
   
     public static function getRespectsReceived($limit = 10)
     {
-        return  QueryBuilder::table('users_settings')->select('user_id')->select('respects_received')->orderBy('respects_received', 'desc')->limit($limit)->get();
+        return  QueryBuilder::table('users_settings')->select('user_id')->select('respects_received')->join('users', 'users_settings.id', '=', 'users.id')->where('users.rank', '<', 4)->orderBy('respects_received', 'desc')->limit($limit)->get();
     }
 
     public static function getCredits($limit = 10)
     {
-        return  QueryBuilder::table('users')->select('id')->select('credits')->orderBy('credits', 'desc')->limit($limit)->get();
+        return  QueryBuilder::table('users')->select('id')->select('credits')->where('rank', '<', 4)->orderBy('credits', 'desc')->limit($limit)->get();
     }
   
     /*
