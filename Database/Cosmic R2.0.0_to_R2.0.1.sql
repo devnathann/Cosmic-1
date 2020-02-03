3c279431fb0c21c386b019ebf8498596ba002a29
devraizer <46655802+devraizer@users.noreply.github.com>
2020-02-03 23:18:40 +0100

    Update Cosmic R2.0.0_to_R2.0.1.sql

--- Database/Cosmic R2.0.0_to_R2.0.1.sql
+++ Database/Cosmic R2.0.0_to_R2.0.1.sql
@@ -1,2 +1,2 @@
 INSERT INTO `website_settings` VALUES ('krews_api_hotel_slug', NULL);
-ALTER TABLE `website_shop_offers MODIFY` currency int(3);
+ALTER TABLE `website_shop_offers` MODIFY currency int(3);
