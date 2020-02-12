13a3823e9280a847340d7f6ab02bff3ee06efd5f
devraizer <46655802+devraizer@users.noreply.github.com>
2020-02-12 21:06:53 +0100

    Update Cosmic R2.0.2_to_R2.0.3.sql

--- Database/Cosmic R2.0.2_to_R2.0.3.sql
+++ Database/Cosmic R2.0.2_to_R2.0.3.sql
@@ -1,1 +1,5 @@
 ALTER TABLE users ADD shuttle_token varchar(64) NULL DEFAULT NULL;
+
+INSERT INTO `website_settings` VALUES ('vip_membership_days', 31);
+INSERT INTO `website_settings` VALUES ('referral_acc_create_days', 14);
+INSERT INTO `website_settings` VALUES ('referral_points_type', 103);
