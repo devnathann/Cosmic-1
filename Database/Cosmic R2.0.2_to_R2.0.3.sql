8534447e7ecd6f018da4e13c17df26a009cc8efe
devraizer <46655802+devraizer@users.noreply.github.com>
2020-02-13 00:25:14 +0100

    Update Cosmic R2.0.2_to_R2.0.3.sql

--- Database/Cosmic R2.0.2_to_R2.0.3.sql
+++ Database/Cosmic R2.0.2_to_R2.0.3.sql
@@ -1,5 +1,20 @@
+-- ----------------------------
+-- Table structure for website_membership
+-- ----------------------------
+
+DROP TABLE IF EXISTS `website_membership`;
+CREATE TABLE `website_membership`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NULL DEFAULT NULL,
+  `old_rank` int(1) NULL DEFAULT NULL,
+  `expires_at` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+
 ALTER TABLE users ADD shuttle_token varchar(64) NULL DEFAULT NULL;
 
 INSERT INTO `website_settings` VALUES ('vip_membership_days', 31);
 INSERT INTO `website_settings` VALUES ('referral_acc_create_days', 14);
 INSERT INTO `website_settings` VALUES ('referral_points_type', 103);
+
