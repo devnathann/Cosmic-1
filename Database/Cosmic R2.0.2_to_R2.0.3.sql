28ab3fa1bae4aa0b7e1e6aaeefccdbacb5e5ea8a
devraizer <46655802+devraizer@users.noreply.github.com>
2020-02-13 01:36:43 +0100

    Update Cosmic R2.0.2_to_R2.0.3.sql

--- Database/Cosmic R2.0.2_to_R2.0.3.sql
+++ Database/Cosmic R2.0.2_to_R2.0.3.sql
@@ -11,10 +11,26 @@ CREATE TABLE `website_membership`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
+-- ----------------------------
+-- Table structure for website_referrals
+-- ----------------------------
 
-ALTER TABLE users ADD shuttle_token varchar(64) NULL DEFAULT NULL;
+DROP TABLE IF EXISTS `website_referrals`;
+CREATE TABLE `website_referrals`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `referral_user_id` int(11) NULL DEFAULT NULL,
+  `ip_address` varchar(56) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `timestamp` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+ALTER TABLE users ADD shuttle_token varchar(128) NULL DEFAULT NULL;
 
 INSERT INTO `website_settings` VALUES ('vip_membership_days', 31);
 INSERT INTO `website_settings` VALUES ('referral_acc_create_days', 14);
 INSERT INTO `website_settings` VALUES ('referral_points_type', 103);
-
+INSERT INTO `website_settings` VALUES ('referral_points', 5);
+INSERT INTO `website_settings` VALUES ('referral_waiting_seconds', 3600);
